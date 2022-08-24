0ad8aca352763132720d5e92742202d3e09bdadd
Subhajit Laha <subhajitlaha.sl@gmail.com>
2022-08-24 10:05:21 +0530

    upgrade

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://cts-pms-lb-1912083700.ap-south-1.elb.amazonaws.com/',
-  register: 'http://cts-pms-lb-1912083700.ap-south-1.elb.amazonaws.com/login/register/',
-  login: 'http://cts-pms-lb-1912083700.ap-south-1.elb.amazonaws.com/login/authenticate/',
-  processPension: 'http://cts-pms-lb-1912083700.ap-south-1.elb.amazonaws.com/ProcessPension/',
-  pensionDetails: 'http://cts-pms-lb-1912083700.ap-south-1.elb.amazonaws.com/ProcessPension/'
+  apiUrl: 'http://zust-pms-1675924804.ap-south-1.elb.amazonaws.com/',
+  register: 'http://zust-pms-1675924804.ap-south-1.elb.amazonaws.com/login/register/',
+  login: 'http://zust-pms-1675924804.ap-south-1.elb.amazonaws.com/login/authenticate/',
+  processPension: 'http://zust-pms-1675924804.ap-south-1.elb.amazonaws.com/ProcessPension/',
+  pensionDetails: 'http://zust-pms-1675924804.ap-south-1.elb.amazonaws.com/ProcessPension/'
 };
 
 /*
